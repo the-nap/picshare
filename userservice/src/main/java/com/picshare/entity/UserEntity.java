8e6caea59a94d411058646b94c08a87193ae0d42
Leonardo Napoli <leonardo.npl.02@gmail.com>
2025-12-11 16:49:10 +0100

    added fields to user entity

--- userservice/src/main/java/com/picshare/entity/UserEntity.java
+++ userservice/src/main/java/com/picshare/entity/UserEntity.java
@@ -1,8 +1,14 @@
 package com.picshare.entity;
 
-import java.sql.Date;
+import java.util.Date;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,8 +23,24 @@ import lombok.ToString;
 public class UserEntity{
 
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(nullable = false)
+  private long id;
+
+  @Column(unique = true, nullable = false)
   private String username;
+
+  @Column(nullable = false)
+  private String password;
+  
+  @Column(unique = true, nullable = false)
   private String email;
+
+  @CreationTimestamp
+  @Column(updatable = false)
   private Date creationDate;
+
+  @UpdateTimestamp
+  private Date updateDate;
 
 }
