43245a1552bece443d7e1556c4eaa24b45adc6a5
Leonardo Napoli <leonardo.npl.02@gmail.com>
2025-12-19 18:55:54 +0100

    added container configuration, still needs volume

--- user-service/src/main/java/com/picshare/service/UserService.java
+++ user-service/src/main/java/com/picshare/service/UserService.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Service;
 
 import com.picshare.repository.UserRepository;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class UserService {
   private final UserRepository userRepository;
 }
