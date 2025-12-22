58a594d0b786e1bc806188524ec5c807567f3216
Leonardo Napoli <leonardo.npl.02@gmail.com>
2025-12-22 10:25:51 +0100

    completed store method

--- storage-service/src/main/java/com/example/storage_service/service/FileSystemStorageService.java
+++ storage-service/src/main/java/com/example/storage_service/service/FileSystemStorageService.java
@@ -1,19 +1,22 @@
 package com.example.storage_service.service;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.storage_service.client.StorageClient;
+import com.example.storage_service.service.exceptions.StorageException;
+import com.example.storage_service.service.util.WebpManager;
 
 import lombok.AllArgsConstructor;
 
@@ -32,17 +35,34 @@ public class FileSystemStorageService implements StorageService{
 
   @Override
   public void store(MultipartFile file, String filename) {
-    try {
-      Path destination = getDirectory(filename);
-    } catch (IOException e) {
-      // TODO
-    }
+      Path destination = createDirectory(filename);
+      Path[] paths = getPaths(destination);
+      try(InputStream inputStream = file.getInputStream();
+          OutputStream outputStreamMedia = Files.newOutputStream(paths[0], StandardOpenOption.CREATE);
+          OutputStream outputStreamThumbnail = Files.newOutputStream(paths[1], StandardOpenOption.CREATE)){
+
+        WebpManager.manage(inputStream, outputStreamMedia, outputStreamThumbnail);
+
+          } catch (IOException e) {
+            throw new StorageException("Error while writing file", e);
+          }
 
   }
 
-  private Path getDirectory(String filename) throws IOException {
+  private Path[] getPaths(Path destination) {
+      Path[] paths = new Path[2];
+      paths[0] = destination.resolve("media");
+      paths[1] = destination.resolve("thumbnail");
+      return paths;
+  }
+
+  private Path createDirectory(String filename){
     Path destination = this.mediaRoot.resolve(Paths.get(filename)).normalize().toAbsolutePath();
-    Files.createDirectories(destination);
+    try {
+      Files.createDirectories(destination);
+    } catch (IOException e) {
+      throw new StorageException("File cannot be saved");
+    }
     return destination;
   }
 
