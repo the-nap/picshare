bd1d292cee3266774f3c95048f86191df57dfe79
Leonardo Napoli <leonardo.npl.02@gmail.com>
2025-12-24 19:32:42 +0100

    added post-service to compose

--- post-service/src/main/java/com/picshare/post_service/entity/PostEntity.java
+++ post-service/src/main/java/com/picshare/post_service/entity/PostEntity.java
@@ -3,7 +3,6 @@ package com.picshare.post_service.entity;
 import java.util.Date;
 
 import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
