850d2574baf23c85741600c7e0bef36e3b0b8831
Leonardo Napoli <leonardo.npl.02@gmail.com>
2026-01-10 22:27:51 +0100

    add routes and related components

--- picshare-frontend/src/app/app.routes.ts
+++ picshare-frontend/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { Feed } from './feed/feed';
+import { Search } from './search/search';
+import { Profile } from './profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'feed',
+    pathMatch: 'full'
+  },
+  {
+    path: 'feed',
+    title: 'Personal Feed',
+    component: Feed
+  },
+  {
+    path: 'search',
+    title: 'Search Pics',
+    component: Search
+  },
+  {
+    path: 'user',
+    title: 'User Profile',
+    component: Profile,
+  }
+];
