75595d8f2cb6a5d1843890f1049c70371a696b67
Leonardo Napoli <leonardo.npl.02@gmail.com>
2026-01-15 16:26:03 +0100

    included CardModule in post

--- picshare-frontend/src/app/post/post.ts
+++ picshare-frontend/src/app/post/post.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card'
 
 @Component({
   selector: 'app-post',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './post.html',
   styleUrl: './post.css',
 })
 export class Post {
+
+  image : string = "test";
 
 }
