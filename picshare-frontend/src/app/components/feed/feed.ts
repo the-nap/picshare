c72ddedffe8e63720de28c1bf976b37227bc54a2
Leonardo Napoli <leonardo.npl.02@gmail.com>
2026-01-21 18:49:46 +0100

    fixed feed data fetching

--- picshare-frontend/src/app/components/feed/feed.ts
+++ picshare-frontend/src/app/components/feed/feed.ts
@@ -1,11 +1,13 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, effect, inject, input } from '@angular/core';
 import { Gallery } from '../gallery/gallery';
 import { FeedService } from './feed.service';
 import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-feed',
-  imports: [Gallery, AsyncPipe],
+  imports: [Gallery, AsyncPipe, MatProgressSpinnerModule],
   templateUrl: './feed.html',
   styleUrl: './feed.css',
 })
@@ -13,10 +15,12 @@ export class Feed {
   userId = input.required<string>();
   service = inject(FeedService);
 
-  resourceId$: any;
+  resourceIds$!: Observable<string[]>;
 
-  ngOnInit() {
-    this.resourceId$ = this.service.getFeed(this.userId());
+  constructor() {
+    effect(() => {
+      this.resourceIds$ = this.service.getFeed(this.userId())
+    });
   }
 
 }
