83a816041b5b70bcb398804b3950c0e61e89f2cc
Leonardo Napoli <leonardo.npl.02@gmail.com>
2026-01-21 18:43:41 +0100

    fixed post data fetching

--- picshare-frontend/src/app/components/post/post.service.ts
+++ picshare-frontend/src/app/components/post/post.service.ts
@@ -3,23 +3,19 @@ import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { environment } from "../../../environments/environment";
 import { PostModel } from "../../models/post.model";
+import { UserModel } from "../../models/user.model";
 
 @Injectable({providedIn: "root"})
 export class PostService {
 
-  apiUrl: string = environment.apiUrl + "image/";
+  apiUrl: string = environment.apiUrl + "posts/";
   private http = inject(HttpClient);
 
   getPost(id: number): Observable<PostModel>{
     return this.http.get<PostModel>(this.apiUrl + id)
   }
 
-  getUsername(id: string): Observable<string> {
-    return this.http.get(`${this.apiUrl}user/` + id, { responseType: 'text' });
-  }
-
-  getDescription(id: string): Observable<string> {
-    return this.http.get(`${this.apiUrl}description/` + id, { responseType: 'text' });
-
+  getUser(id: number): Observable<UserModel>{
+    return this.http.get<UserModel>(this.apiUrl + id)
   }
 }
--- picshare-frontend/src/app/components/post/post.ts
+++ picshare-frontend/src/app/components/post/post.ts
@@ -1,25 +1,35 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, effect, inject, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card'
-import { NgOptimizedImage } from '@angular/common';
+import { AsyncPipe, NgOptimizedImage } from '@angular/common';
 import { PostService } from './post.service';
 import { PostModel } from '../../models/post.model';
+import { Observable, switchMap } from 'rxjs';
+import { UserModel } from '../../models/user.model';
+import { UserService } from '../user/user.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 
 @Component({
   selector: 'app-post',
-  imports: [MatCardModule, NgOptimizedImage],
+  imports: [MatCardModule, MatProgressSpinnerModule, NgOptimizedImage, AsyncPipe],
   templateUrl: './post.html',
   styleUrl: './post.css',
 })
 
 export class Post {
+  postService = inject(PostService);
+  userService = inject(UserService);
+
   resourceId = input.required<number>();
-  service = inject(PostService);
 
-  post: PostModel;
+  post$!: Observable<PostModel>;
+  user$!: Observable<UserModel>;
 
-  ngOnInit(){
-    this.service.getPost(this.resourceId()).subscribe((resource) => {
-                                                   this.post = resource;
+  constructor() {
+    effect(() => {
+      this.post$ = this.postService.getPost(this.resourceId())
     });
+    this.user$ = this.post$.pipe(
+      switchMap(post => this.userService.getUser(post.userId))
+    );
   }
 }
