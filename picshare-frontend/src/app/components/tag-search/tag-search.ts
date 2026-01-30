72c585e10350dd2432b92b880f139e439aa614ba
Leonardo Napoli <leonardo.npl.02@gmail.com>
2026-01-30 16:04:08 +0100

    implemented rxResource in tag-search component

--- picshare-frontend/src/app/components/tag-search/tag-search.ts
+++ picshare-frontend/src/app/components/tag-search/tag-search.ts
@@ -1,8 +1,8 @@
-import { Component, effect, inject, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Gallery } from '../gallery/gallery';
 import { PreviewService } from '../preview/preview.service';
-import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-tag-search',
@@ -16,14 +16,10 @@ export class TagSearch {
 
   service = inject(PreviewService);
 
-  postIds$!: Observable<string[]>
-
-  constructor() {
-    effect(() => {
-      this.postIds$ = this.service.getByTag(this.tag());
-    });
-  }
-
-
-
+  postIds = rxResource<number[], string>({
+    params: () => ( this.tag() ),
+    stream: ({params}) => {
+      return this.service.getByTag(params);
+    }
+  });
 }
