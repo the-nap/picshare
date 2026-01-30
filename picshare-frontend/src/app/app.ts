eb871c9af66285335f0726116672166d02acc7c1
Leonardo Napoli <leonardo.npl.02@gmail.com>
2026-01-30 19:36:43 +0100

    completed auth0 setup

--- picshare-frontend/src/app/app.ts
+++ picshare-frontend/src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Navbar } from './components/navbar/navbar';
+import { AuthService } from '@auth0/auth0-angular';
+import { LoginButtonComponent } from './components/auth0/login-button.component';
+import { LogoutButtonComponent } from './components/auth0/logout-button.component';
+import { ProfileComponent } from './components/auth0/profile.component';
+import { AsyncPipe } from '@angular/common';
+import { Search } from './components/search/search';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar],
+  imports: [Navbar, LoginButtonComponent, LogoutButtonComponent, ProfileComponent, AsyncPipe, Search],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('picshare-frontend');
+  protected auth = inject(AuthService);
 }
