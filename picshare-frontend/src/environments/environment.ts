88c93064bc086583dd77010216830193eb5d5d48
Leonardo Napoli <leonardo.npl.02@gmail.com>
2026-01-30 18:45:37 +0100

    added needed dependencies for auth0

--- picshare-frontend/src/environments/environment.ts
+++ picshare-frontend/src/environments/environment.ts
@@ -1,4 +1,10 @@
 export const environment = {
-  apiUrl: 'http://gateway:8080/api/'
 
+  apiUrl: 'http://gateway:8080/api/',
+
+  production: false,
+  auth0: {
+    domain: 'dev-fvsw1ye5xesa2ta0.eu.auth0.com',
+    clientId: 'k7Yuq5JGexg6Gf332KTBCgF0ISbQx7ax',
+  },
 };
